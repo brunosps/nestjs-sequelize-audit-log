e655d8fee05abb45de0136b3e53e0c6feaaadd2f
Bruno Santos <brunosilva.santos@rodobens.com.br>
2025-06-16 17:55:35 -0300

    Adicionado export para createAuditSoapClient

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ export { PayloadDetailsService } from './audit-log-core/services/payload-details
 // Event module exports
 export { AuditLogEvent } from './audit-log-event/decorators/audit-log-event.decorator';
 
+// Integration module exports
+export {
+  SoapClient,
+  createAuditSoapClient,
+} from './audit-log-integration/utils/soap-client.utils';
+export { AuditLogSoapClientService } from './audit-log-integration/services/audit-log-soap-client.service';
+export { SoapClientUtilsProvider } from './audit-log-integration/providers/soap-client-utils.provider';
+
 // Utility exports
 export { extractClientIp } from './utils/ip';
 
